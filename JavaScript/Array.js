a6d8ee5f03db52527e1be208335a3e840835d3e2
dakshsaini05 <dakshsince2005@gmail.com>
2024-11-14 17:22:05 +0530

    Arrays

---
+++ JavaScript/Array.js
@@ -0,0 +1,34 @@
+let arr = [1,2,3,4,5]
+
+// accessing array using for loop
+// for(let idx=0;idx<arr.length;idx++) {
+//     console.log(arr[idx])
+// }
+
+// accessing array using for-of loop
+// for(let a of arr) {
+//     console.log(a)
+// }
+
+// accessing array using for-in loop
+// for(let a in arr) {
+//     console.log(arr[a])
+// }
+
+// Find the average marks given in an array
+// let marks = [85,97,44,37,76,60]
+// let sum = 0
+// for(let m of marks) {
+//     sum += m
+// }
+// let avg = sum / marks.length
+// console.log(`Average of total marks is: ${avg}`);
+
+// Array methods 
+// push -> add elements at the end of the array
+// pop -> delete & return that element from the end of the array 
+let arr2 = [1,2,3,4,5]
+// arr2.push(6)
+console.log(arr2)
+arr2.pop(6) 
+console.log(arr2)
